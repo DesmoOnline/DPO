df8d22b8c310230c44155f644841a7ad058957b0
BJMack <bjmack@BJMacks-MacBook-Air.local>
2026-07-19 17:56:26 +0800

    Hardcode Firebase config for App Hosting

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // The user's Firebase config from their Google Business Account
 export const firebaseConfig = {
-  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY,
-  authDomain: (import.meta as any).env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: (import.meta as any).env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: (import.meta as any).env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: (import.meta as any).env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: (import.meta as any).env.VITE_FIREBASE_APP_ID,
-  measurementId: (import.meta as any).env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDzOel-toXx7oT3DJ1GqgerW-X6dIdsyh0",
+  authDomain: "desmoproductsonline.firebaseapp.com",
+  projectId: "desmoproductsonline",
+  storageBucket: "desmoproductsonline.firebasestorage.app",
+  messagingSenderId: "684470912687",
+  appId: "1:684470912687:web:9b31a83fb8217cd691f65a",
+  measurementId: "G-6MM332FX5Y"
 };
 
 let app;
